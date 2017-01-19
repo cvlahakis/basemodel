51ac1fde76ce5ab2ddbce4ce8d5cd1152e0fd89c
Costas Vlahakis <4thirstyc4m3l@gmail.com>
2017-01-18 21:53:15 -0500

    Add some unit tests

---
+++ spec/dbInterface.js
@@ -0,0 +1,13 @@
+module.exports = {
+    insertOne: function(table, attributes, cb) {
+        cb();
+    },
+
+    updateOne: function(table, where, attributes, cb) {
+        cb();
+    },
+
+    removeOne: function(table, where, cb) {
+        cb();
+    }
+};
---
+++ spec/helpers/reporter.js
@@ -0,0 +1,11 @@
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(new SpecReporter({
+    spec: {
+        displayPending: true
+    },
+    summary: {
+        displayDuration: false
+    }
+}));
